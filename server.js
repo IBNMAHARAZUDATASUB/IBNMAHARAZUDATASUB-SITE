b51acd4e863129b7c715d45f7ffda50404a04e7c
IBNMAHARAZUDATASUB <Ibnmaharazuupgrade@gmail.com>
2026-05-14 20:39:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the root folder
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
-  res.send('It Works! Your site is running on Render.');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
