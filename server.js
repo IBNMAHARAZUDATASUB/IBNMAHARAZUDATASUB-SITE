a8fd5299cf857085eb197dcb97fd171e0a4aa22a
IBNMAHARAZUDATASUB <Ibnmaharazuupgrade@gmail.com>
2026-05-15 11:58:56 +0100

    fix sever for render

--- server.js
+++ server.js
@@ -8,12 +8,14 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+// Serve static files from root
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
+// Send index.html for all routes
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
